4d7c23cb152ed41afaf1f65b5667dd1a4cc2af89
Farley de Souza R <Farley de Souza R@DESKTOP-CN59BUT>
2019-06-14 16:39:33 -0300

    Parâmetros REST Operator

--- js/app/Controllers/NegociacaoController.js
+++ js/app/Controllers/NegociacaoController.js
@@ -8,11 +8,11 @@ class NegociacaoController{
         this._inputValor = $('#valor');
          
         
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        
         this._listaNegociacoes = new  Bind(
             new ListaNegociacao(),
-            this._negociacoesView ,
-            ['adiciona','esvazia']  
+            new NegociacoesView($('#negociacoesView')) ,
+            'adiciona','esvazia'  
         
         )
         
@@ -33,11 +33,11 @@ class NegociacaoController{
        // this._negociacoesView.update(this._listaNegociacoes);
 
         //Mensagem de Sucesso
-        this._mensagemView = new MensagemView($('#mensagemView'));
+        
         this._mensagem = new Bind(
           new Mensagem(),
-          this._mensagemView,
-            ['texto']
+          new MensagemView($('#mensagemView')),
+            'texto'
         )
         
     //     ProxyFactory.create(new Mensagem(),
